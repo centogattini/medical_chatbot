360a62fdaf5d0cea3cbd4f289740325f20458fc5
Stepan Kabanov <phoenixgenera@gmail.com>
2022-10-07 12:36:38 +0300

    changed columns name in  doctors_datebase.py

--- doctors_database.py
+++ doctors_database.py
@@ -5,11 +5,13 @@ class Database():
         self.table_name = 'timetable'
         self.con = sqlite3.connect(path)
         self.cur = self.con.cursor()
-    def get_avaliable_time(self,doctor,date):
-        res = self.cur.execute(f'SELECT * FROM {self.timetable} AS T WHERE T.date = "{date}" and T.doctor = "{doctor}"')
+    def get_avaliable_time(self,profession,date):
+        res = self.cur.execute(f'SELECT * FROM {self.timetable} AS T WHERE T.date = "{date}" and T.profession = "{profession}"')
         if res is None:
             return None
-        times = res.fetchone()[2:]
-        return times
-    def set_appointment(self,time,doctor,date): 
-        self.cur.execute(f'UPDATE timtable AS T SET T.{time} = 1 WHERE T.doctor = "{doctor}" AND T.date = "{date}"')
+        return res.fetchone()
+    def set_appointment(self,time,profession,date): 
+        self.cur.execute(f'UPDATE timtable AS T SET T.{time} = 1 WHERE T.profession = "{profession}" AND T.date = "{date}"')
+    def get_all_doctors(self,):
+        res = self.cur.execute(f'SELECT DISTINCT T.profession, T.name FROM timetable')
+        return res.fetchone()
