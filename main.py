30a95c4a49b05d5a39b6e90bc06db8c1e0547f84
Stepan Kabanov <phoenixgenera@gmail.com>
2022-10-07 15:44:47 +0300

    added raw main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from doctors_database import Database
+#from doctors_classifier import DrClassifier
+def main():
+    db = Database()
+    db.get_all_doctors()
+    #clf = DrClassifier()
+
+
+
+
+if __name__ == '__main__':
+    main()
