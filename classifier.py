5d16da54991854e06c4a377ac57a776479e756d1
Stepan Kabanov <phoenixgenera@gmail.com>
2022-10-14 12:30:25 +0300

    added classifier.py (interface) and input_output.py

---
+++ classifier.py
@@ -0,0 +1,9 @@
+from collections import List
+class Classifier():
+    def __init__(self,doctors:List[str]):
+        self.doctors = doctors
+        pass
+    def classify_symptoms(self,)->str:
+        pass
+    def identify_name(self,name)->str:
+        pass
---
+++ input_output.py
@@ -0,0 +1,8 @@
+class InpOut:
+    def __init__(self,):
+        pass
+    def read(self,):
+        return input()
+    def write(self,text):
+        print(text)
+        
