d19ad48f0d69856a0692ebdd9144f12aeb20f1c6
Stepan Kabanov <phoenixgenera@gmail.com>
2022-10-14 13:20:51 +0300

    changed classifier.py

--- classifier.py
+++ classifier.py
@@ -1,9 +1,13 @@
 from collections import List
+from consts import CONST
 class Classifier():
     def __init__(self,doctors:List[str]):
         self.doctors = doctors
         pass
     def classify_symptoms(self,)->str:
         pass
-    def identify_name(self,name)->str:
+    def identify_name_or_profession(self,name):
+        # return tuple (str, tag)
+        # str:string -- name or profession
+        # tag: CONST variable
         pass
