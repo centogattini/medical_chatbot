32e322f87b6fac6ea0790eb0caa3a8f385ca79d8
Stepan Kabanov <phoenixgenera@gmail.com>
2022-10-21 15:29:16 +0300

    added dialoger

--- dialoger.py
+++ dialoger.py
@@ -1,11 +1,42 @@
 from input_output import InpOut
-from doctors_database import Database
-class Dialog:
-    def __init__(self,io:InpOut,db:Database):
+from doctors_database import Database # hh:mm:ss
+from classifier import Classifier
+from consts import CONST
+class Dialoger:
+    def __init__(self,io:InpOut,db:Database,clf:Classifier):
         self.io = io
         self.db = db
+        self.clf = clf
+        self.set_of_doctors = self.db.get_all_doctors()
+
+    # start dialog
+    def start_dialog(self,):
+        ans = self.ask_dyk_doctor()
+        if ans == CONST.yes:
+            ans = self.ask_name_or_profession()
+            ans, tag = self.clf.identify_name_or_profession(ans)
+            while tag == CONST.error:
+                ans, tag = self.clf.identify_name_or_profession(ans)
+            if tag == CONST.name:
+                ans = self.ask_choose_date(ans)
+                ans = self.ask_time(ans)
+                self.say_bye()
+            elif tag == CONST.profession:
+                ans = self.ask_choose_doctor(ans)
+                ans = self.ask_choose_date(ans)
+                ans = self.ask_time(ans)
+                self.say_bye()
+        else:
+            ans, tag = self.ask_symptoms()
+            if tag == CONST.error:
+                ans = self.ask_unknown_symptoms()
+            else:
+                ans = self.ask_choose_doctor()
+    # when bot didn't understood symptoms and asks a person to set an apointment to therpaist
+    def ask_unknown_symptoms(self,):
+        pass
     # ask a person does he know the name of a doctor 
-    def ask_dyk_doctor(self,):
+    def ask_dyk_doctor(self,)->str:
         pass
     # ask a person to write down the doctor's name
     def ask_name_or_profession(self,):
