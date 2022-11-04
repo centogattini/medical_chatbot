e26188774a5dc7bc5564cfdf683b16b8a741dd70
Stepan Kabanov <phoenixgenera@gmail.com>
2022-11-05 00:32:27 +0300

    bebra

--- TelegramAPI.py
+++ TelegramAPI.py
@@ -1,15 +1,44 @@
 import telebot
-import consts
+from telebot import types
+from classifier import Classifier
 
-telebot.apihelper.ENABLE_MIDDLEWARE = True
-bot = telebot.TeleBot('5514115973:AAEUGJilFHRu1NE8dqLh1p3RpXSKbCWAtPQ')
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-	bot.send_message(message.from_user.id,""" Здравствуйте. Это медицинский бот для 
-    записи к врачу. ВЫ знаете к кому обратиться?""")
-	keyboard = telebot.types.ReplyKeyboardMarkup(True,False)
-	keyboard.add('Да')
-	keyboard.add('Нет')
-	
-bot.infinity_polling(none_stop=True, interval=1)
+class TelegramBot:
+	def __init__(self, TOKEN, created_classifier, database):
+		telebot.apihelper.ENABLE_MIDDLEWARE = True
+		self.bot = telebot.TeleBot(TOKEN)
+		self.classifier = created_classifier
+		self.database = self.database
+
+	def dialog(self):
+		@self.bot.message_handler(commands=['start'])
+		def start_message(message):
+
+			keyboard = types.ReplyKeyboardMarkup(row_width=2)	
+			btn1 = types.KeyboardButton(text = 'Да')
+			btn2 = types.KeyboardButton(text = 'Нет')
+			keyboard.add(btn1,btn2)
+			self.bot.send_message(message.from_user.id,""" Здравствуйте. Это медицинский бот для 
+			записи к врачу. Вы знаете к кому обратиться?""",reply_markup=keyboard)
+
+		@self.bot.message_handler(commands=['text'])
+		def first_question(message):
+			if message.text == 'Да':	
+				self.bot.send_message(message.from_user.id, "Напишите нам имя или профессию врача")
+				self.bot.register_next_step_handler(message, name_or_prof)
+
+			elif message.text == 'Нет':
+				self.bot.send_message(message.from_user.id, "Пожалуйста, напишите нам свои симптомы")
+				self.bot.register_next_step_handler(message, symp_recognition)
+
+		@self.bot.message_handler(commands=['text'])
+		def name_or_prof(message):
+			self.bot.send_message(message.from_user.id,'1')
+
+		@self.bot.message_handler(commands=['text'])
+		def symp_recognition(message):
+			self.bot.send_message(message.from_user.id,'2')
+
+
+		self.bot.infinity_polling(none_stop=True, interval=1)
+		##class with write and read
